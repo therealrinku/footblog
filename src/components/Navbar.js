00d4c32607892fd413eab87aee68546f15d04429
rinkuuu <backupfb2032@gmail.com>
2021-01-04 09:10:50 +0545

    navbar styleshhet removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav
+      className="navbar"
+      style={{ fontSize: "22px", color: "white", position: " absolute" }}
+    >
       <ul>
         <p>footblog</p>
       </ul>
