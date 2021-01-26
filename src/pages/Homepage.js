bffed0f1a8d4cbc12a46218cb22a040cde637336
rinkuuu <backupfb2032@gmail.com>
2021-01-26 07:59:53 +0545

    mapped through posts

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -6,8 +6,7 @@ const Homepage = () => {
     {
       blogTitle: "Poch lands PSG job apparently",
       blogImage: "https://bit.ly/3ccRudU",
-      blogDescription:
-        "https://images.squarespace-cdn.com/content/v1/5442b6cce4b0cf00d1a3bef2/1553287602197-R2B39P80V3IROJRE898K/ke17ZwdGBToddI8pDm48kJB_XQ3Dc5SllUdWbukQo69Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpx6nEhjlnWL4Jp2yRUKnAHfAtKyAEGaEsOmaOT_zZoS2TtcLgPBsTCXYa-xKfKtauE/minimalist-blogs.jpg?format=500w",
+      blogDescription: "Psg new manager is Poch!",
       blogPostedDate: "24 Nov 2020",
     },
     {
@@ -50,27 +49,17 @@ const Homepage = () => {
   return (
     <div className="homepage">
       <Navbar />
-      {posts.map}
-      <PostCard
-        blogDescription="Lorem ipsum dolor sit amet, consectetur
-             adipiscing elit."
-        blogImage={posts[0].blogImage}
-        blogTitle={posts[0].blogTitle}
-        blogPostedDate={posts[0].blogPostedDate}
-      />
-
-      <PostCard
-        blogDescription={posts[1].blogDescription}
-        blogImage={posts[1].blogImage}
-        blogTitle={posts[1].blogTitle}
-        blogPostedDate={posts[1].blogPostedDate}
-      />
-      <PostCard
-        blogDescription={posts[2].blogDescription}
-        blogImage={posts[2].blogImage}
-        blogTitle={posts[2].blogTitle}
-        blogPostedDate={posts[2].blogPostedDate}
-      />
+      {posts.map((post, i) => {
+        return (
+          <PostCard
+            key={i}
+            blogDescription={post.blogDescription}
+            blogImage={post.blogImage}
+            blogTitle={post.blogTitle}
+            blogPostedDate={post.blogPostedDate}
+          />
+        );
+      })}
     </div>
   );
 };
