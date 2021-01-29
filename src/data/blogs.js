0b247c922621935c54eeec541e264b870ba44263
rinkuuu <backupfb2032@gmail.com>
2021-01-29 13:55:52 +0545

    description added to all blogs

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -43,7 +43,41 @@ const blogs = [
     blogTitle: "Spurs thrashed by struggling Liverpool side.",
     blogShortDescription:
       "After difficult few weeks Reds have won convincingly at spurs.",
-    blogDescription: [],
+    blogDescription: [
+      `
+    Liverpool returned to form in impressive fashion as the Premier League champions secured a 
+    convincing victory at Tottenham Hotspur.
+    After a run of five league games without a win - four of those without a goal - 
+    Liverpool moved back into the top four as they discovered their cutting edge in 
+    style at the expense of an uninspiring Spurs.
+    Spurs, who lost striker Harry Kane to an ankle injury at half-time, 
+    had an early Son Heung-min strike ruled out for offside before Liverpool's 
+    threat was rewarded with their first league goal in 482 minutes in first-half stoppage time.
+    `,
+      `
+    Brazil striker Roberto Firmino took advantage of hesitation between 
+    Eric Dier and Hugo Lloris to tap in Sadio Mane's low cross from close range.
+    France keeper Lloris was at fault again two minutes after the break when 
+    he pushed Mane's shot into the path of right-back Trent Alexander-Arnold, who finished emphatically.
+    Pierre-Emile Hojbjerg's 20-yard strike gave Spurs hope almost 
+    immediately but Liverpool were always in control of an entertaining second period.
+    `,
+      `
+    Jose Mourinho spent much of the second half bellowing instructions at 
+    his Spurs players in an attempt to spark a reaction - 
+    but it was to no avail as they produced a lacklustre performance.
+    Liverpool always carried a greater threat once 
+    Son's early finish was ruled out for VAR but it was a moment of indecision between 
+    Dier and Lloris that opened the door for Firmino right at the end of the first half.
+    `,
+      `
+    The lack of threat was perhaps less surprising after the loss of Kane, 
+    who twice received treatment for ankle knocks before eventually succumbing 
+    to the second one on his left ankle which was sustained in an innocuous challenge with Henderson.
+    Mourinho suggested after the match Kane may miss "a few weeks" and on this 
+    evidence Spurs will struggle to be anywhere near their best without their talisman and leading marksman.
+    `,
+    ],
     blogDate: "27 Jan 2021",
     blogImage: "https://bit.ly/2NLEthb",
   },
@@ -52,7 +86,41 @@ const blogs = [
     blogTitle: "Manchester united back to default settings?",
     blogShortDescription:
       "Red devils has missed an opportunity to move to top of the table after dissapointing home loss.",
-    blogDescription: [],
+    blogDescription: [
+      `
+      Ole Gunnar Solskjaer has joked many times this season that his 
+      Manchester United side like to do things the hard way, and they couldn’t have made life more 
+      difficult for themselves during a dreadful 2-1 loss at home to Sheffield United on Wednesday night.
+      It was always likely that if they kept falling behind in games, their combination of luck, 
+      grit and determination would eventually prove unable to bail them out of trouble. 
+      The only surprise was that it happened at Old Trafford against the bottom team in the Premier League.
+      `,
+      `
+      While there was genuine hope they could mount one of their now-famous
+      comebacks when Harry Maguire powered home a second-half equaliser, 
+      United were ultimately undone by abysmal defending, with Oliver Burke firing home the winner 
+      with 16 minutes to go after the hosts had squandered a succession of chances to clear the ball.
+      `,
+      `
+      Solskjaer had been hoping that Sunday's dramatic FA Cup win over bitter rivals 
+      Liverpool would prove a pivotal moment in the season, 
+      as it gave his players the win over a big-six side that 
+      they had been craving since the start of the 2020-21 campaign.
+      However, United have little hope of winning trophies if they do 
+      not address their maddening propensity for starting games slowly, 
+      as they did once again on Wednesday night.
+      `,
+      `
+      Of course, one defeat isn’t enough to start writing off United's title chances, 
+      in the same way that a string of wins wasn’t 
+      enough to back them to finish top of the pile. 
+      However, no matter what their targets are, they 
+      cannot afford more anaemic performances like this.
+      In a season of unpredictable and surprising results 
+      anything is still possible but we will now see what
+      Solskjaer's side is made of, as this is a true test of character.
+      `,
+    ],
     blogDate: "28 Jan 2021",
     blogImage: "https://bit.ly/2KXebHC",
   },
@@ -61,7 +129,44 @@ const blogs = [
     blogTitle: "Manchester city flying high.",
     blogShortDescription:
       "Leaders City are in 13 games winning streak at the moment.",
-    blogDescription: [],
+    blogDescription: [
+      `
+      Fueled by a sparkling attacking display, Manchester City's players powered to 
+      the top of the Premier League for the first time this season — 
+      and even the opposition are stopping to admire their work.
+      In a comical exhibit for VAR's ever-lengthening highlights reel, 
+      West Bromwich Albion's defenders virtually stopped in their tracks 
+      and looked on as Joao Cancelo curled a shot into the top corner 
+      for the second of City's goals in a 5-0 rout on Tuesday.
+      `,
+      `
+      Make it seven straight wins in the league — and 11 in all competitions — 
+      for City in an ominous run of results. 
+      City became the ninth side to finish a day 
+      in first place this season, the most within a single top-flight campaign since 1986-87.
+      Even if Manchester United reclaims the lead on 
+      Wednesday by beating Sheffield United, City — 
+      on its longest winning run in more than three 
+      years — looks to be the team to beat.
+      `,
+      `
+      City were without Kevin de Bruyne, Sergio Aguero and Gabriel Jesus - introduced at 5-0 - 
+      and had no out-and-out forward in their team, and still could have scored more goals in an 11th straight win.
+      Phil Foden had already had a shot turned on to the 
+      post when the superb Gundogan controlled a pass over 
+      the top, swivelled and clipped the ball into the far corner in one fluid movement.
+      `,
+      `Then came the game's bizarre second goal.
+      Sian Massey-Ellis flagged Bernardo Silva offside as he was played in down the right. His cutback to 
+      Cancelo was chipped into the top corner, with the flag still raised and some players stationary.`,
+      `
+      A VAR check confirmed Bernardo was in fact onside and the goal stood, to Allardyce's disbelief.
+      "Listen we all make mistakes," he said. "She made a mistake and put her flag up too soon.
+      "You can't tell me that rubbish about playing to the whistle, 
+      that's nonsense, if the flag goes up you naturally stop. 
+      But saying that, she made one mistake. We made far more today."
+      `,
+    ],
     blogDate: "28 Jan 2021",
     blogImage: "https://bit.ly/39xhwH7",
   },
@@ -70,7 +175,42 @@ const blogs = [
     blogTitle: "Pogba remains silent on contract situation.",
     blogShortDescription:
       "Pogba has refused to talk about contract situation at Mufc.",
-    blogDescription: [],
+    blogDescription: [
+      `
+      Paul Pogba has confirmed he is planning to hold talks with the Man Utd board over his contract.
+      The French midfielder's future has been the subject of debate after his agent, Mino Raiola, 
+      claimed last month that he was "unhappy" at Old Trafford.
+      Since then Pogba has produced some of his best form in a United shirt 
+      and looks happier in his surroundings.
+      Nevertheless, his contract is due to expire at the end of 
+      next season, meaning United will need to make a decision 
+      on his future this summer or risk losing him for nothing.
+      `,
+      `
+      "Now my objective, my goal is to 
+      win something that’s all I’m thinking about is to win, 
+      bring my energy, do the best I can to help the team the most.
+      "That's all I'm thinking of. The contract is not important, just to win."
+      Pogba has never commented on Raiola's claims 
+      made at the start of December, where the agent 
+      stated the 28-year-old had "no intentions" of extending his United deal.
+      Raiola told Tuttosport at the time: "Paul is unhappy. 
+      He is no longer able to express himself as he would like and as expected of him.
+      `,
+      `
+      Discussing Pogba's future to ESPN , United boss Ole Gunnar Solskjaer said earlier this month: 
+      "Paul has been here now for a while and, of course, he wants to win more trophies.
+      "He's got a very, very good attitude to training and playing football.
+      "He loves football, he's ambitious, he wants to have success -
+       like most of the others that are in the squad."
+       That is now Pogba's short-term goal, and whether United collect 
+       any silverware this season could prove to be a deciding factor in whether he stays put.
+      `,
+      `
+      Pogba added: "From last year and this year I would say it’s surprising [United's progress] 
+      but we were expecting to get closer, that's the goal, the challenge to get to the top.
+      `,
+    ],
     blogDate: "24 Jan 2021",
     blogImage: "https://bit.ly/3iUn4OS",
   },
@@ -79,7 +219,40 @@ const blogs = [
     blogTitle: "Kane injured for 2 games.",
     blogShortDescription:
       "Harry Kane came limping off in first half against Liverpool.",
-    blogDescription: [],
+    blogDescription: [
+      `
+      There were three changes at halftime of 
+      Liverpool’s 3-1 away win over Tottenham Hotspur on Thursday, and two of them will raise eyebrows.
+      The one that won’t is the removal of Serge Aurier,
+       who was poor in the first half and one of the 
+       main culprits of Roberto Firmino’s opener just before halftime.
+      `,
+      `
+      The exits of Harry Kane and Joel Matip, however, will be monitored by both teams.
+      Kane spent two spells on the turf in the first half, 
+      leaving the pitch after several minutes of treatment early in the half but returning.
+      “Two ankles,” said Jose Mourinho after the game. 
+      “The first was a bad tackle, I think Thiago. The second 
+      I didn’t know well but two injuries, the second one worse 
+      than the first one and a few weeks. How many? I don’t know. … 
+      There are some players that 
+      you can’t replace and almost every tea has this kind of player. 
+      We have to fight against it and that’s it.”
+      `,
+      `
+      Spurs’ star striker was then injured going for an aerial ball 
+      with Jordan Henderson and did not return to the pitch after halftime.
+      He’s faced a number of injury absences in the past 
+      few years but has been a lethal finisher when healthy. 
+      Kane has 19 goals and 14 assists in 28 appearances this season.
+      `,
+      `
+      Mourinho lamented the loss of his irreplaceable talisman 
+      as he provided an update on the potential double ankle injury the 27-year-old sustained
+      "Two ankles, the first was a bad tackle, 
+      it was Thiago, the second one I didn't know well," Mourinho told BBC Radio 5 Live .
+      `,
+    ],
     blogDate: "28 Jan 2021",
     blogImage: "https://bit.ly/3iXtgWc",
   },
