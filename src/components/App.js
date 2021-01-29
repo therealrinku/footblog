43719c596278d107c1a9ddf404507653e4d2d7fc
rinkuuu <backupfb2032@gmail.com>
2021-01-29 14:02:00 +0545

    404 page added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import Homepage from "../pages/Homepage";
 import { BrowserRouter, Route } from "react-router-dom";
 import PostDetailsPage from "../pages/PostDetailsPage";
+import Page_404 from "../pages/404Page";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Homepage} />
       <Route path="/post/:post_id" exact component={PostDetailsPage} />
+      <Route component={Page_404} />
     </BrowserRouter>
   );
 };
---
+++ src/pages/404Page.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+const Page_404 = () => {
+  return (
+    <div className="404--page">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Page_404;
