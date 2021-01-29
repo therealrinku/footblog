33a8de1fa56ad2fdfa749f118a438a00e48ddb14
rinkuuu <backupfb2032@gmail.com>
2021-01-29 08:44:25 +0545

    data addeD

---
+++ src/data/blogs.js
@@ -0,0 +1,50 @@
+const blogs = [
+  {
+    id: 1,
+    blogTitle: "Tuchel announced as new Chelsea boss.",
+    blogShortDescription:
+      "Thomas Tuchel , the former PSG man has been appointed new Chelsea Manager.",
+    blogDescription: ``,
+    blogDate: "24 Jan 2021",
+  },
+  {
+    id: 2,
+    blogTitle: "Spurs thrashed by struggling Liverpool side.",
+    blogShortDescription:
+      "After difficult few weeks Reds have won convincingly at spurs.",
+    blogDescription: ``,
+    blogDate: "27 Jan 2021",
+  },
+  {
+    id: 3,
+    blogTitle: "Manchester united back to default settings?",
+    blogShortDescription:
+      "Red devils has missed an opportunity to move to top of the table after dissapointing home loss.",
+    blogDescription: ``,
+    blogDate: "28 Jan 2021",
+  },
+  {
+    id: 4,
+    blogTitle: "Manchester city flying high.",
+    blogShortDescription:
+      "Leaders City are in 13 games winning streak at the moment.",
+    blogDescription: ``,
+    blogDate: "28 Jan 2021",
+  },
+  {
+    id: 5,
+    blogTitle: "Pogba remains silent on contract situation.",
+    blogShortDescription:
+      "Pogba has refused to talk about contract situation at Mufc.",
+    blogDescription: ``,
+    blogDate: "24 Jan 2021",
+  },
+  {
+    id: 6,
+    blogTitle: "Kane injured for 2 games.",
+    blogShortDescription:
+      "Harry Kane came limping off in first half against Liverpool.",
+    blogDescription: ``,
+    blogDate: "28 Jan 2021",
+  },
+];
