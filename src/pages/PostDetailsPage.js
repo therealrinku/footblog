ed6263247f4d2f56d8d854faad9fd305db1212df
rinkuuu <backupfb2032@gmail.com>
2021-01-29 09:20:11 +0545

    dynamicaaly displaying details page

--- src/pages/PostDetailsPage.js
+++ src/pages/PostDetailsPage.js
@@ -1,61 +1,23 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import blogs from "../data/blogs";
 
-const PostDetailsPage = () => {
+const PostDetailsPage = ({ match }) => {
+  const blogPost =
+    blogs.filter((blog) => blog.id === match.params.post_id)[0] || [];
+
   return (
     <div className="post--details-page">
       <Navbar />
 
       <section>
         <div>
-          <h4>Tuchel is new Chelsea boss</h4>
-          <p>26 Jan 2020</p>
-          <img src="https://bit.ly/3ccRudU" alt="test" />
+          <h4>{blogPost.blogTitle}</h4>
+          <p>{blogPost.blogDate}</p>
+          <img src={blogPost.blogImage} alt="test" />
         </div>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget nunc
-          scelerisque viverra mauris in aliquam. Tortor aliquam nulla facilisi
-          cras fermentum odio eu feugiat pretium. Purus viverra accumsan in
-          nisl. Interdum velit euismod in pellentesque massa placerat duis.
-          Mollis aliquam ut porttitor leo a diam sollicitudin tempor id. Eget mi
-          proin sed libero enim sed faucibus. Duis convallis convallis tellus id
-          interdum. Luctus accumsan tortor posuere ac ut consequat semper
-          viverra. Laoreet id donec ultrices tincidunt arcu non sodales neque
-          sodales. Enim ut tellus elementum sagittis vitae. Orci ac auctor augue
-          mauris augue neque. Interdum velit laoreet id donec ultrices tincidunt
-          arcu. Potenti nullam ac tortor vitae purus. Elit sed vulputate mi sit
-          amet mauris commodo. Cum sociis natoque penatibus et magnis dis
-          parturient montes. Adipiscing diam donec adipiscing tristique risus
-          nec feugiat in fermentum. Odio aenean sed adipiscing diam donec
-          adipiscing tristique. Vitae turpis massa sed elementum tempus.
-        </p>
-
-        <p>
-          Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt
-          vitae. Id diam vel quam elementum pulvinar. Sed felis eget velit
-          aliquet. Suscipit tellus mauris a diam maecenas. Lorem mollis aliquam
-          ut porttitor leo a diam sollicitudin. Semper risus in hendrerit
-          gravida rutrum quisque non tellus orci. Elementum nisi quis eleifend
-          quam adipiscing vitae proin sagittis. In dictum non consectetur a.
-          Tristique senectus et netus et malesuada fames ac. Nisl vel pretium
-          lectus quam id leo in. Pulvinar mattis nunc sed blandit libero
-          volutpat sed cras. Justo eget magna fermentum iaculis. Vitae tempus
-          quam pellentesque nec. Fermentum et sollicitudin ac orci phasellus
-          egestas tellus.
-        </p>
-
-        <p>
-          Libero id faucibus nisl tincidunt eget nullam non nisi. Urna id
-          volutpat lacus laoreet non curabitur gravida arcu ac. Arcu risus quis
-          varius quam quisque id diam vel quam. Maecenas ultricies mi eget
-          mauris. Non tellus orci ac auctor augue mauris augue. Mi in nulla
-          posuere sollicitudin. Id cursus metus aliquam eleifend mi in. Risus
-          nullam eget felis eget nunc lobortis mattis aliquam. Id donec ultrices
-          tincidunt arcu non sodales. Cras pulvinar mattis nunc sed blandit
-          libero volutpat.
-        </p>
+        <p>{blogPost.blogDescription}</p>
       </section>
     </div>
   );
